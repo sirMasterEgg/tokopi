ea7baa4378f802a6bccd901341d80ed142cad975
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2023-05-17 01:21:35 +0700

    initial commit

---
+++ src/main/java/com/mbokmu/tokopi/TokopiApplication.java
@@ -0,0 +1,13 @@
+package com.mbokmu.tokopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokopiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokopiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mbokmu/tokopi/TokopiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbokmu.tokopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokopiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
