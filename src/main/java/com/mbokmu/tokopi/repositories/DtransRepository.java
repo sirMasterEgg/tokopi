69ef609309374e0dc4f37a92688abc274e80ac56
Ignatius Odi <ignodi@gmail.com>
2023-05-17 05:28:12 +0700

    add repositories

---
+++ src/main/java/com/mbokmu/tokopi/repositories/DtransRepository.java
@@ -0,0 +1,7 @@
+package com.mbokmu.tokopi.repositories;
+
+import com.mbokmu.tokopi.models.Dtrans;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DtransRepository extends JpaRepository<Dtrans, Integer> {
+}
---
+++ src/main/java/com/mbokmu/tokopi/repositories/MenuRepository.java
@@ -0,0 +1,7 @@
+package com.mbokmu.tokopi.repositories;
+
+import com.mbokmu.tokopi.models.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository<Menu, Integer> {
+}
