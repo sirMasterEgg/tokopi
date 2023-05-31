70e15abcf19480571668800f381e2ee2826e3065
VictorShielo <82816338+sirMasterEgg@users.noreply.github.com>
2023-06-01 01:47:23 +0700

    get product udh selesaiiii

---
+++ src/main/java/com/mbokmu/tokopi/AppConfig.java
@@ -0,0 +1,14 @@
+package com.mbokmu.tokopi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
+
--- src/main/java/com/mbokmu/tokopi/controllers/PageController.java
+++ src/main/java/com/mbokmu/tokopi/controllers/PageController.java
@@ -1,10 +1,22 @@
 package com.mbokmu.tokopi.controllers;
 
+import com.mbokmu.tokopi.models.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class PageController {
+
+    final MenuController menuController;
+
+    @Autowired
+    public PageController(MenuController menuController) {
+        this.menuController = menuController;
+    }
 
     @RequestMapping("/")
     public String index() {
@@ -15,7 +27,9 @@ public class PageController {
         return "order";
     }
     @RequestMapping("/menu")
-    public String menu() {
+    public String menu(Model model) {
+        List<Menu> menus = menuController.findAll();
+        model.addAttribute("menus", menus);
         return "menu";
     }
 }
