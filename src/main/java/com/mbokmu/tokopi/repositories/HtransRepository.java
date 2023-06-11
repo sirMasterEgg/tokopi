52106f20441e6371f8a94b0a75debc1b83a9bdfd
Ignatius Odi <ignodi@gmail.com>
2023-06-11 16:06:52 +0700

    add htrans query to findLast

--- src/main/java/com/mbokmu/tokopi/repositories/HtransRepository.java
+++ src/main/java/com/mbokmu/tokopi/repositories/HtransRepository.java
@@ -2,6 +2,9 @@ package com.mbokmu.tokopi.repositories;
 
 import com.mbokmu.tokopi.models.Htrans;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface HtransRepository extends JpaRepository<Htrans, Integer> {
+    @Query("SELECT htrans FROM Htrans htrans ORDER BY htrans.id DESC LIMIT 1")
+    Htrans findLast();
 }
